4856f2df85ddf0fbd7132415571d416713723d8f
Edmund Parsons <edmundparsons@live.co.uk>
2021-06-07 17:24:42 +0100

    model & routes update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :reviews
   has_many :logs
+  has_many :waters, through: :logs
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :waters, only: [:show, :index] do
-    resources :reviews, only: [:create, :index]
+    resources :reviews, only: :create
   end
   resources :logs, only: :index
   resources :reviews, only: :destroy
