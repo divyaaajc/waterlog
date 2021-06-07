494db40fae4a568be1267589b14d13044885fbc9
Edmund Parsons <edmundparsons@live.co.uk>
2021-06-07 16:07:37 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :waters, only: [:show, :index] do
+    resources :reviews, only: [:create, :index]
+  end
+  resources :logs, only: :index
+  resources :reviews, only: :destroy
 end
