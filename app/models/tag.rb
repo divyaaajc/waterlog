febaf865fcf1d43cd3dcdf953f047d75fa31013b
Divya Chouhan <divyajchouhan@icloud.com>
2021-06-07 16:47:07 +0100

    validations and associations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  has_many :water_tags
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/water.rb
+++ app/models/water.rb
@@ -1,2 +1,9 @@
 class Water < ApplicationRecord
+  has_many :reviews
+  has_many :water_tags
+  has_many :logs
+
+  validates :name, presence: true, uniqueness: true
+  validates :brand, :country, :description, :source, presence: true
+  validates :ph, presence: true, numericality: true
 end
