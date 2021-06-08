97f2fc22f193c2054ca221200f5a84ec46fdd158
Edmund Parsons <edmundparsons@live.co.uk>
2021-06-08 16:57:15 +0100

    geocode show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_135127) do
+ActiveRecord::Schema.define(version: 2021_06_08_145519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 2021_06_08_135127) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   add_foreign_key "logs", "users"
