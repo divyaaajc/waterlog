599c2c2cf8a657fa181ea51647b2b3d436d3d86a
Edmund Parsons <edmundparsons@live.co.uk>
2021-06-08 12:27:58 +0100

    action & views added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(strong_params)
+    @water = Water.find(params[:water_id])
+    @review.water = @water
+    @review.user = current_user
+    if @review.save
+      redirect_to water_path(@water)
+    else
+      render water_path(@water) #render :new
+    end
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(strong_params)
+      redirect_to water_path(@water)
+    else
+      render water_path(@water) #render :edit
+    end
+  end
+
+  def destroy
+    @review = Reivew.find(params[:id])
+    @review.destroy
+    redirect_to water_path(@review.water),  notice: 'Review was successfully deleted' #look into
+  end
+
+  private
+
+  def strong_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -1,2 +1,10 @@
 class WatersController < ApplicationController
+
+  def index
+    @waters = Water.all
+  end
+
+  def show
+    @water = Water.find(params[:id])
+  end
 end
