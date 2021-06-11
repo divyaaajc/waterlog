d85045eb138637122b16c25f9e8757e0242433b1
Edmund Parsons <edmundparsons@live.co.uk>
2021-06-11 12:12:25 +0100

    random waters page basic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,27 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :about, :random]
 
   def home
     @waters = Water.all.sample(9)
   end
 
   def about
+  end
+
+  def random
+    random_water
+    @review = Review.new
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = [
+      {
+        lat: @water.latitude,
+        lng: @water.longitude
+      }]
+  end
+
+  private
+
+  def random_water
+    @water = Water.all.sample(1).first
   end
 end
