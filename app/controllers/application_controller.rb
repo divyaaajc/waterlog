f014221be8322cc02e762dbc008d7a2aff2d17d4
Divya Chouhan <divyajchouhan@icloud.com>
2021-06-17 12:33:48 +0100

    redirection and flashes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :store_user_location!, if: :storable_location?
+
   add_flash_types :success
 
   def configure_permitted_parameters
@@ -11,7 +13,22 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
   end
 end
